e6fca8d1b7f36accc654bf12cd4cf8f10c183bf0
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-02-02 14:16:37 +0300

    IndexOf string function explained

---
+++ Introduce C#/AnotherBuiltInFunctions/AnotherBuiltInFunctions/Program.cs
@@ -0,0 +1,13 @@
+﻿string kelime = "adana";
+//kelime içerisinde aranacak harfin yerlerini bulan kod parçası
+//örnek a harfi arandığında 1. ve 5. sırada olduğunu söylesin.
+
+int index = kelime.IndexOf('a', 2);
+//Console.WriteLine(index);
+int aranacakIndex = 0;
+while (kelime.IndexOf('a', aranacakIndex) != -1)
+{
+    int bulunanIndex = kelime.IndexOf('a', aranacakIndex);
+    aranacakIndex = bulunanIndex + 1;
+    Console.WriteLine($"{kelime} kelimesinde a harfi {bulunanIndex}. indexte!");
+}
