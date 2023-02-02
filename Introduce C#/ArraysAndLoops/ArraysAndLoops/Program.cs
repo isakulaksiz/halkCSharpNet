e1fa15c29be52cd7a38805dfd3b8c7808092ba1b
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-02-02 12:01:54 +0300

    Loops, Arrays & Generic Lists

---
+++ Introduce C#/ArraysAndLoops/ArraysAndLoops/Program.cs
@@ -0,0 +1,57 @@
+﻿int[] numbers = { 55, 23, -4, 19, 26, 123, 0 };
+int minimum = numbers[0];
+for (int index = 1; index < numbers.Length; index++)
+{
+	if (minimum > numbers[index])
+	{
+		minimum = numbers[index];
+	}
+}
+
+Console.WriteLine(minimum);
+//Console.WriteLine(numbers.ToList().Min());
+//Console.WriteLine(numbers.ToList().Max());
+
+string[] cities = { "Istanbul", "Cankırı", "Van", "Yozgat", "Bitlis" };
+Console.WriteLine("Aradığınız il:");
+string searchingValue = Console.ReadLine();
+bool isCityFinded = false;
+for (int i = 0; i < cities.Length; i++)
+{
+	if (searchingValue == cities[i])
+	{
+		isCityFinded = true;
+		break;
+	}
+}
+
+foreach (var city in cities)
+{
+	if (searchingValue == city)
+	{
+		isCityFinded = true;
+		break;
+	}
+}
+
+foreach (var city in cities)
+{
+	if (city.Length < 5)
+	{
+		continue;
+	}
+	Console.WriteLine(city);
+}
+
+if (!isCityFinded)
+{
+	Console.WriteLine($"{searchingValue} şehirlerde yok!");
+}
+else
+{
+	Console.WriteLine($"{searchingValue} şehirlerde var");
+}
+
+//1. Yeni bir array oluştur, bir önceki array'in eleman sayısından bir fazla olsun.
+//2. Eski array'in tüm elemanlarını yeni array'a taşı (kopyala)
+//3. Yeni ili (örnek:Bilecik) yeni array'in son elemanına ekle
---
+++ Introduce C#/CollectionsAndGeneric/CollectionsAndGeneric/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+
+List<string> words = new List<string>();
+
+//boxing:
+object value = "Deneme";
+//unboxing:
+//string strValue = (string)value;
+//int error = (int)value;
+
+
+//Generic olmasaydı:
+ArrayList arrayList = new ArrayList
+{
+    15
+};
+int firstNumber = (int)arrayList[0];
+
+//Generic iyi ki var :)
+List<int> numberList = new List<int>
+{
+    23,
+    -12,
+    9,
+    542,
+    98,
+    25,
+    15
+};
+int firstNumberOfList = numberList[0];
+
+if (numberList.Contains(15))
+{
+    Console.WriteLine("15 koleksiyonda var");
+}
+
+numberList.Sort();
+Console.WriteLine("Sıralanmış liste:");
+foreach (var item in numberList)
+{
+    Console.WriteLine(item);
+}
+
+
+
+
