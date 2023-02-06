80d20c09247ed86f89b6a1ceed11075f0ac47d1a
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-02-06 11:09:26 +0300

    creating & using generic types

---
+++ Introduce C#/GenericTypes/GenericTypes/Point.cs
@@ -0,0 +1,95 @@
+﻿namespace GenericTypes
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public void SetPosition(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Reset()
+        {
+            X = 0;
+            Y = 0;
+        }
+
+
+
+
+    }
+
+    public class DoublePoint
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public void SetPosition(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Reset()
+        {
+            X = 0.0;
+            Y = 0.0;
+        }
+    }
+
+    public class GeneralPoint
+    {
+        public object X { get; set; }
+        public object Y { get; set; }
+
+        public void SetPosition(object x, object y)
+        {
+            X = x;
+            Y = y;
+        }
+
+
+
+        public void Reset()
+        {
+            X = 0.0;
+            Y = 0.0;
+        }
+    }
+
+    public class GenericPoint<T> where T : struct, IComparable
+    {
+        public T X { get; set; }
+        public T Y { get; set; }
+
+        public void SetPosition(T x, T y)
+        {
+            X = x;
+            Y = y;
+        }
+
+
+
+        public void Reset()
+        {
+            X = default!;
+            Y = default(T);
+        }
+
+        public bool isEqual()
+        {
+            return X.CompareTo(Y) == 0;
+        }
+
+        //public Random MyProperty { get; set; } = default!;
+
+    }
+
+
+
+
+
+}
---
+++ Introduce C#/GenericTypes/GenericTypes/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GenericTypes;
+
+Console.WriteLine("Hello, World!");
+GeneralPoint generalPoint = new GeneralPoint
+{
+    X = 15
+};
+generalPoint.SetPosition("dkd", false);
+//int value = (int)generalPoint.X;
+
+GenericPoint<decimal> point = new GenericPoint<decimal>
+{
+    X = 1.0M,
+    Y = 0.8M
+};
+point.Reset();
+
+GenericPoint<double> genericPoint = new GenericPoint<Double>();
+
+if (isEqual(15.0, 15.0))
+{
+    Console.WriteLine("eşittir");
+}
+else
+{
+    Console.WriteLine("eşit değildir");
+}
+
+bool isEqual<T, W>(T value1, W value2) where T : struct, IComparable
+                                       where W : struct, IComparable
+{
+    return value1.CompareTo(value2) == 0;
+}
