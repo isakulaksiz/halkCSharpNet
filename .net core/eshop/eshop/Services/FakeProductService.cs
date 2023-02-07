0ef9ce3719b590aaaabbf04f8df707d7ddbf9048
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-02-07 16:18:13 +0300

    eshop via MVC Template

---
+++ .net core/eshop/eshop/Services/FakeProductService.cs
@@ -0,0 +1,24 @@
+﻿using eshop.Models;
+
+namespace eshop.Services
+{
+    public class FakeProductService
+    {
+        private List<Product> products;
+        public FakeProductService()
+        {
+            products = new()
+            {
+                new() { Id=1, Description="Test Açıklama", DiscountRate=0.2, ImageUrl="https://cdn.dsmcdn.com/ty709/product/media/images/20230131/17/270762905/660823832/1/1_org.jpg", Name="Test Name", Price=1},
+                new() { Id=2, Description="Test Açıklama 2", DiscountRate=0.2, ImageUrl="https://cdn.dsmcdn.com/ty709/product/media/images/20230131/17/270762905/660823832/1/1_org.jpg", Name="Test Name 2", Price=1},
+                new() { Id=3, Description="Test Açıklama 3", DiscountRate=0.2, ImageUrl="https://cdn.dsmcdn.com/ty709/product/media/images/20230131/17/270762905/660823832/1/1_org.jpg", Name="Test Name 3", Price=1},
+                new() { Id=4, Description="Test Açıklama 4", DiscountRate=0.2, ImageUrl="https://cdn.dsmcdn.com/ty709/product/media/images/20230131/17/270762905/660823832/1/1_org.jpg", Name="Test Name 4", Price=1},
+
+
+            };
+        }
+
+        public IEnumerable<Product> GetProducts() => products;
+
+    }
+}
