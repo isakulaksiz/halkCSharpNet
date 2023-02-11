ffce2c5d4b6cc74ba3d0c8a22e6ae1ae75fd4d54
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-02-11 15:32:57 +0300

    adding JWT Bearer ASP.NET Core package

---
+++ .net core/eshop/eshop.API/Controllers/AccountsController.cs
@@ -0,0 +1,39 @@
+﻿using eshop.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace eshop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public AccountsController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel userLogin)
+        {
+
+            var user = _userService.ValidateUser(userLogin.UserName, userLogin.Password);
+            if (user != null)
+            {
+                var claims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName ),
+                    new Claim(ClaimTypes.Role, user.Role)
+
+                };
+
+
+            }
+            return Ok();
+
+        }
+    }
+}
--- .net core/eshop/eshop.API/Program.cs
+++ .net core/eshop/eshop.API/Program.cs
@@ -17,6 +17,23 @@ builder.Services.AddScoped<IProductRepository, EFProductRepository>();
 var connectionString = builder.Configuration.GetConnectionString("db");
 builder.Services.AddDbContext<EshopDbContext>(option => option.UseSqlServer(connectionString));
 
+builder.Services.AddCors(option => option.AddPolicy("allow", builder =>
+{
+    /*
+     * http://www.halkbank.com.tr
+     * https://www.halkbank.com.tr
+     * https://api.halkbank.com.tr
+     * https://www.halkbank.com.tr:8282
+     * 
+     * 
+     *  http://www.halkbank.com.tr/krediler
+     * 
+     */
+    builder.AllowAnyOrigin();
+    builder.AllowAnyMethod();
+    builder.AllowAnyHeader();
+}));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,6 +44,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("allow");
 
 app.UseAuthorization();
 
